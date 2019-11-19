09fd6678179aa20815c2c7046cbce09d0d9d1cb5
Ruan-Miguel <ruanmiguel14@hotmail.com>
2019-11-19 13:08:05 -0300

    initial structure

---
+++ app.js
@@ -0,0 +1,28 @@
+//Modules
+const express = require('express')
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const app = express()
+const user = require('./routes/user')
+const path = require('path')
+
+//Settings
+    //Body Parser
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+
+    //Handlebars
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+
+    //Public
+    app.use(express.static(path.join(__dirname,'public')))
+
+//Routes
+app.use('/user', user)
+
+//Others
+const PORT = 8081
+app.listen(PORT, () => {
+    console.log(`Servidor so o ouro rodando em http://localhost:${PORT}`)
+})
