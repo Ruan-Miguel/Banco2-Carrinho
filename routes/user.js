2c6efef0098fab03f20e6406b282b48de576e707
Ruan-Miguel <ruanmiguel14@hotmail.com>
2019-11-20 10:29:58 -0300

    redirecionamento apos adicao de produto no carrinho foi implementado

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,17 @@ const express= require('express')
 const router = express.Router()
 const productController = require('../controllers/productController')
 
-router.get('/', (req, res) => {
+router.get('/products', (req, res) => {
     productController().then(products => {
         res.render('user/products', {products})
     })
+})
+
+router.get('/addProduct/:id', (req, res) => {
+    const { id } = req.params
+
+    console.log(id)
+    res.redirect('/user/products')
 })
 
 module.exports = router
